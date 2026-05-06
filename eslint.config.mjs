e32eb25671c208469fe1f336df5863358307f762
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2026-05-05 20:50:14 -0400

    Fix config lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
